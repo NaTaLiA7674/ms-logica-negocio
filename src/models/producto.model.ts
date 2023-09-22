5c93a5fef5d1c0814a42d5ed7b809957bf0a1dae
NaTaLiA7674 <130678658+NaTaLiA7674@users.noreply.github.com>
2023-09-22 13:14:01 -0500

    Conexión del proyecto al MySQL

---
+++ src/models/producto.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Producto extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precioVenta: number;
+
+  @property({
+    type: 'string',
+    required: false,
+  })
+  foto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidadDisponible: number;
+
+
+  constructor(data?: Partial<Producto>) {
+    super(data);
+  }
+}
+
+export interface ProductoRelations {
+  // describe navigational properties here
+}
+
+export type ProductoWithRelations = Producto & ProductoRelations;
---
+++ src/models/venta.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Venta extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  numero: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fecha: string;
+
+
+  constructor(data?: Partial<Venta>) {
+    super(data);
+  }
+}
+
+export interface VentaRelations {
+  // describe navigational properties here
+}
+
+export type VentaWithRelations = Venta & VentaRelations;
