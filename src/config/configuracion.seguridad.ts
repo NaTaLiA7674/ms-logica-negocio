4fa989f97810f2b6b4179fa0518e627503338f3d
NaTaLiA7674 <130678658+NaTaLiA7674@users.noreply.github.com>
2023-09-30 12:17:47 -0500

    Creación de estrategia para ms-logica

---
+++ src/config/configuracion.seguridad.ts
@@ -0,0 +1,8 @@
+export namespace ConfiguracionSeguridad {
+  export const menuProductoId = "64fbb35f352cba33ccfdad58";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
+}
